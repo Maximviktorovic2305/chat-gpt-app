2c33492632d4e248c7ea6e357176c22682a37080
Max <max@mail.ru>
2026-07-18 17:13:01 +1000

    Baseline existing database before migrations

---
+++ server/prisma/migrations/20251206000000_init/migration.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
